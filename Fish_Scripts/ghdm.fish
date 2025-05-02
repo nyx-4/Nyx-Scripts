2e061e24323acec378171bd3f8707915b324381c
Nyx <189459385+nyx-4@users.noreply.github.com>
2025-05-02 19:35:10 +0500

    Added simple fish script to Download raw files from GitHub
    Signed-off-by: Nyx <189459385+nyx-4@users.noreply.github.com>

---
+++ Fish_Scripts/ghdm.fish
@@ -0,0 +1,23 @@
+#!/bin/fish
+
+function PrintUsageMsg
+    set_color red
+    echo "Invalid URL"
+    echo "Usage: ./ghdm.fish [GitHub URL]"
+    set_color normal
+    exit 1
+end
+
+function DownloadFromGH
+    string join "/" $argv[1] "/raw.githubusercontent.com" $argv[4] $argv[5] "refs/heads" $argv[7..]
+end
+
+if test (count $argv) -eq 0
+    set URL (read -p "set_color yellow ; echo -n 'Enter URL: '; set_color normal;")
+else
+    set URL $argv
+end
+
+for url in $URL
+    fdm (DownloadFromGH (string split "/" $url))
+end
